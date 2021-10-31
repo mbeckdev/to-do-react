2b7f2f1c8197db6be77b1085ec326acc286e6340
mbeckdev <mbeckdev42@gmail.com>
2021-10-31 15:19:34 -0600

    Add styled components and testing things

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import TestComponent from './components/TestComponent';
+import styled, { ThemeProvider } from 'styled-components';
 
+const darkTheme = {
+  colors: {
+    primary: `yellow`,
+    secondary: `red`,
+    color1Red: `#ffa69e`,
+    color2Yellow: `#faf3dd`,
+    color3Green: `#b8f2e6`,
+    color4LightBlue: `#aed9e0`,
+    color5Black: `#1c1d22`,
+    // color5Black: `#4b4f5b`,
+    // color5Black: `#5e6472`,
+    // https://colorhex.net/5e6472 has great shades
+  },
+};
+const lightTheme = {
+  colors: {
+    primary: `lightyellow`,
+    secondary: `lightred`,
+    color1Red: `#11a69e`,
+    color2Yellow: `#11f3dd`,
+    color3Green: `#11f2e6`,
+    color4LightBlue: `#11d9e0`,
+    color5Black: `#111122`,
+    // color5Black: `#4b4f5b`,
+    // color5Black: `#5e6472`,
+    // https://colorhex.net/5e6472 has great shades
+  },
+};
+
+const WrapperApp = styled.section`
+  background: ${(props) => props.theme.colors.color5Black};
+`;
 function App() {
+  let useLightTheme = false;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={useLightTheme ? lightTheme : darkTheme}>
+      {/* <ThemeProvider theme={darkTheme}> */}
+      <WrapperApp>
+        <div className="App">
+          <header className="App-header">
+            <p>headdderrrrrr</p>
+          </header>
+          <main>
+            <p>main stuff here</p>
+            <TestComponent />
+          </main>
+          <footer>footer here</footer>
+        </div>
+      </WrapperApp>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/TestComponent.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function TestComponent() {
+  const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: ${(props) => props.theme.colors.color2Yellow};
+  `;
+  // Create a Wrapper component that'll render a <section> tag with some styles
+  const Wrapper = styled.section`
+    padding: 4em;
+    background: ${(props) => props.theme.colors.color5Black};
+    /* background: papayawhip; */
+    color: blue;
+    /* color: ${(props) => props.theme.colors.primary}; */
+  `;
+
+  return (
+    <div>
+      <Wrapper>
+        <Title>Hello worrrrld!</Title>
+      </Wrapper>
+    </div>
+  );
+}
+
+export default TestComponent;
